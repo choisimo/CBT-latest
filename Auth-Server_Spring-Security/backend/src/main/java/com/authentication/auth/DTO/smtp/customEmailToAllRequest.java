0b974db41e1eafb0159ccf82dc0fee29f961547e
Yoo-SH <ads137485@jbnu.ac.kr>
2025-05-18 21:07:20 +0900

    delete: 불필요한 파일 삭제

--- Auth-Server_Spring-Security/backend/src/main/java/com/authentication/auth/DTO/smtp/customEmailToAllRequest.java
+++
@@ -1,9 +0,0 @@
-package com.career_block.auth.DTO.smtp;
-
-import lombok.Data;
-
-@Data
-public class customEmailToAllRequest {
-    private String title;
-    private String content;
-}
--- Auth-Server_Spring-Security/backend/src/main/java/com/authentication/auth/DTO/users/loginRequest.java
+++
@@ -1,5 +0,0 @@
-package com.authentication.auth.DTO.users;
-
-import lombok.Data;
-
-public record LoginRequest(String userId, String password) {}
