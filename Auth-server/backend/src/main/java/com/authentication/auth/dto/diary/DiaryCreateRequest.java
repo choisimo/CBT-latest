20d38f75c6af1e74c50a0c53de1d45d5f750ce36
nodove <150008602+choisimo@users.noreply.github.com>
2025-06-21 16:17:05 +0900

    DiaryCreateRequest.java 업데이트

--- Auth-server/backend/src/main/java/com/authentication/auth/dto/diary/DiaryCreateRequest.java
+++ Auth-server/backend/src/main/java/com/authentication/auth/dto/diary/DiaryCreateRequest.java
@@ -1,4 +1,11 @@
 package com.authentication.auth.dto.diary;
 
-public record DiaryCreateRequest(String title, String content) {
+import jakarta.validation.constraints.NotBlank;
+
+public record DiaryCreateRequest(
+        @NotBlank(message = "제목은 필수 입력 항목입니다.")
+        String title,
+        @NotBlank(message = "내용은 필수 입력 항목입니다.")
+        String content
+) {
 }
