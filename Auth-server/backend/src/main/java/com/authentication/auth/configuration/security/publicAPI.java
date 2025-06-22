59363d496116137bd6ae465aadb2949350e326f0
Yoo-SH <ads137485@jbnu.ac.kr>
2025-06-23 03:31:20 +0900

    feat: public endpoint cors 설정 추가

--- Auth-server/backend/src/main/java/com/authentication/auth/configuration/security/publicAPI.java
+++ Auth-server/backend/src/main/java/com/authentication/auth/configuration/security/publicAPI.java
@@ -21,7 +21,7 @@ public class publicAPI {
             "/login", "/public", "/api/public", "/errorPage", "/notExist", "/unauthorized", "/swagger-ui.html", "/v2/api-docs", "/swagger-resources", "/webjars", "/swagger-resources/configuration/ui", "/swagger-resources/configuration/security"
             ,"/oauth2/authorization/google", "/oauth2/authorization/google", "/oauth2/authorization/facebook", "/oauth2/authorization/github", "/oauth2/authorization/linkedin", "/oauth2/authorization/instagram", "/oauth2/authorization/twitter", "/oauth2/authorization/yahoo", "/oauth2/authorization/spotify", "/oauth2/authorization/amazon", "/oauth2/authorization/microsoft", "/oauth2/authorization/okta", "/oauth2/authorization/slack"
             ,"/oauth2/callback/google", "/oauth2/callback/google", "/oauth2/callback/facebook", "/oauth2/callback/github", "/oauth2/callback/linkedin", "/oauth2/callback/instagram", "/oauth2/callback/twitter", "/oauth2/callback/yahoo", "/oauth2/callback/spotify", "/oauth2/callback/amazon", "/oauth2/callback/microsoft", "/oauth2/callback/okta", "/oauth2/callback/slack",
-            "/user/login", "/user/register", "/user/verify", "/user/forgetPassword", "/user/resetPassword", "/user/verifyResetPassword", "/user/verifyEmail", "/user/resendVerificationEmail"
+            "/user/login", "/user/register", "/user/verify", "/user/forgetPassword", "/user/resetPassword", "/user/verifyResetPassword", "/user/verifyEmail", "/user/resendVerificationEmail", "/api/diary", "/api/diary/analyze", "/api/diary/responses", "/api/diary/health"
     );
 
     public boolean checkRequestAPI(HttpServletRequest request){
